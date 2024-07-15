039def67cde2b9bcdb4ee410c03711c2620ea021
Kate Kos <katsiaryna_kasavets@senla.eu>
2024-07-15 19:29:32 +0300

    Add .idea and target to .gitignore and remove them from the repository

---
+++ src/test/java/org/example/UnreadableFile.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.io.IOException;
+
+public class UnreadableFile extends Throwable {
+    public UnreadableFile(String s, IOException e) {
+    }
+}
---
+++ src/test/java/org/example/page_objects/LoginPage.java
@@ -0,0 +1,2 @@
+package org.example.page_objects;public class LoginPage {
+}
